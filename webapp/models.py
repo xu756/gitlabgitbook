0fb0be2cdcb1c01c3671163145e90262e4748a01
xu756 <756334744@qq.com>
2025-04-24 16:14:31 +0800

    update

--- webapp/models.py
+++ webapp/models.py
@@ -54,3 +54,31 @@ choices = (
 #         # 返回公司名为company_name的所有数据
 #         self.company_name = company_name
 #         return Stock.objects.filter(company_name=company_name)
+
+
+# 轮播图
+class Banner(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, verbose_name='标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name='轮播图', max_length=200)
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    sort = models.IntegerField(default=1, verbose_name='顺序')
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        return self.title
+    
+# 留言
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, verbose_name='姓名')
+    email = models.EmailField(verbose_name='邮箱')
+    message = models.CharField(max_length=500, verbose_name='留言')
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name='留言时间')
+    class Meta:
+        verbose_name = '留言'
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        return self.name
